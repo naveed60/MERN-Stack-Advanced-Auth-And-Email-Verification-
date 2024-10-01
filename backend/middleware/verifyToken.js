97a79bf971b27680c7864b6d143adf740d805f39
Naveed Ali <advmiannaveedali@gmail.com>
2024-10-01 18:08:39 +0500

    update token verificaton

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -2,9 +2,11 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
 	const token = req.cookies.token;
+	const JWT_SECRET ="efd8e3bff8d78e2750b27fea14290c53ecfff4796d93a9e48c2adc7f292560f8"
+	console.log("JWT Secret:", JWT_SECRET);
 	if (!token) return res.status(401).json({ success: false, message: "Unauthorized - no token provided" });
 	try {
-		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		const decoded = jwt.verify(token, JWT_SECRET);
 
 		if (!decoded) return res.status(401).json({ success: false, message: "Unauthorized - invalid token" });
 
