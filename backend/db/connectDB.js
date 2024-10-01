fcc9877f1033f7cc7a3b6c9e707094f72175aa1d
Naveed Ali <advmiannaveedali@gmail.com>
2024-10-01 18:07:53 +0500

    update auth controller for verification

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
+	const MONGO_URI ="mongodb+srv://imnaveed60:mna12345@cluster0.kzc40.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 	try {
-		console.log("mongo_uri: ", process.env.MONGO_URI);
-		const conn = await mongoose.connect(process.env.MONGO_URI);
+		console.log("mongo_uri: ", MONGO_URI);
+		const conn = await mongoose.connect(MONGO_URI);
 		console.log(`MongoDB Connected: ${conn.connection.host}`);
 	} catch (error) {
 		console.log("Error connection to MongoDB: ", error.message);
