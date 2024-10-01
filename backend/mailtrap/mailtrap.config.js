b549d4cc729b85ce5b605906e71bf53bc7715a4e
Naveed Ali <advmiannaveedali@gmail.com>
2024-10-01 18:10:04 +0500

    update pakeges

--- backend/mailtrap/mailtrap.config.js
+++ backend/mailtrap/mailtrap.config.js
@@ -3,12 +3,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const MAILTRAP_ENDPOINT ="https://send.api.mailtrap.io/";
+const MAILTRAP_TOKEN="863c35416f1b2f42bcb0139b525429a7";
 export const mailtrapClient = new MailtrapClient({
-	endpoint: process.env.MAILTRAP_ENDPOINT,
-	token: process.env.MAILTRAP_TOKEN,
+	endpoint: MAILTRAP_ENDPOINT,
+	token: MAILTRAP_TOKEN,
 });
 
 export const sender = {
-	email: "imnaveed60@gmail.com",
-	name: "Naveed ali",
+	email: "hello@demomailtrap.com",
+	name: "Mail Trap",
 };
